3135b179fc2481a878b5f7346bbbdedd56574840
samLynux <sambisnes@gmail.com>
2024-01-08 11:54:04 +0700

    Added SCharacter & Input
    -Character
    -Move Forward & Turn

---
+++ Source/UnrealTestProject/UnrealTestProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealTestProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALTESTPROJECT_API AUnrealTestProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
